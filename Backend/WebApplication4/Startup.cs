8d82e93460486cfe3be393a7b8bdf53f5764b566
Ramtin Asef-Nakhai <con-rana@kamstrup.com>
2020-03-15 15:54:04 +0100

    142 changes

--- Backend/WebApplication4/Startup.cs
+++ Backend/WebApplication4/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using WebApplication4.Data;
 
 namespace WebApplication4
@@ -32,11 +33,20 @@ namespace WebApplication4
             services.AddDbContext<HaandvaerkerContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("WebApplication4Context")));
 
-            
+            //    Cycles in Domaine model
+            //    https://stackoverflow.com/questions/58846765/cycle-in-database
+            //https://stackoverflow.com/questions/55787018/net-core-3-preview-4-addnewtonsoftjson-is-not-defined
+            services.AddControllers().AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            //services.AddDbContext<F20ITONKASPNETCore31MicroserviceBackendContext>(options =>
+            //        options.UseSqlServer(Configuration.GetConnectionString("F20ITONKASPNETCore31MicroserviceBackendContext")));
+            services.AddDbContext<HaandvaerkerContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("F20ITONKASPNETDockerConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, HaandvaerkerContext db)
         {
             if (env.IsDevelopment())
             {
@@ -57,7 +67,8 @@ namespace WebApplication4
 
             using var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope();
             var context = serviceScope.ServiceProvider.GetRequiredService<HaandvaerkerContext>();
-            context.Database.EnsureCreated();
+            //context.Database.EnsureCreated();
+            db.Database.Migrate();
         }
     }
 }
